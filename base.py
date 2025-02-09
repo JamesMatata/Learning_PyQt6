ceeb6a2d15acb45bdf939c220df09ecc59798bcd
James Matata <jamesmatatamule@gmail.com>
2025-02-09 23:41:46 +0300

    first commit

---
+++ base.py
@@ -0,0 +1,17 @@
+from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtGui import QIcon
+import sys
+
+class window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(200,200, 400, 250)
+        self.setWindowTitle("PyQt6 ")
+        self.setWindowIcon(QIcon('images/icon.png'))
+
+
+app = QApplication(sys.argv)
+window = window()
+window.show()
+sys.exit(app.exec())
